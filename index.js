3abb762a8881ac23269fb77a3df8824dccfd5e92
xstartstaticx <daniel.reiller@icloud.com>
2022-08-24 17:33:46 +0200

    added 2nd solution

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ console.log(emp1.fullname);
 console.log("---------2. Email-----------");
 // 2. Email
 console.log(emp1.email);
+
+// BONUS: 2nd way with methods
+
+// class Employee {
+//   constructor(firstname, lastname) {
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//   }
+
+//   createFullName() {
+//     console.log(`${this.firstname} ${this.lastname}`);
+//   }
+//   createEmail() {
+//     console.log(
+//       `${this.firstname.toLowerCase()}.${this.lastname.toLowerCase()}@company.com`
+//     );
+//   }
+// }
+
+// const student1 = new Employee("Daniel", "Reiller");
+// console.log(student1);
+
+// console.log("---------1. Full Name-----------");
+// student1.createFullName();
+// console.log("---------2. Email-----------");
+// student1.createEmail();
