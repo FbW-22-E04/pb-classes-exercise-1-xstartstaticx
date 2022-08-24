043b06dfd572dad8a686d16cf74b0883b5ca601f
xstartstaticx <daniel.reiller@icloud.com>
2022-08-24 15:54:19 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    // Complete the code!
+    // this.email = email;
+    this.email = `${this.firstname.toLowerCase()}.${this.lastname.toLowerCase()}@company.com`;
+    this.fullname = `${this.firstname} ${this.lastname}`;
+  }
 }
+const newName = new Employee("Lala", "Christl");
+
+console.log("---------1. Full Name-----------");
+// 1. Full name
+console.log(newName.fullname);
+console.log("---------2. Email-----------");
+// 2. Email
+console.log(newName.email);
